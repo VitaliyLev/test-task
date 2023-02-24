1fec44bfbdc192fb6bee6d07b6e7cce9ca2b2ab8
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2023-02-24 13:37:18 +0200

    fix http

--- src/services/MakeupAPI.js
+++ src/services/MakeupAPI.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const makeupApi = axios.create({
-  // baseURL: 'https://makeup-api.herokuapp.com/api/v1',
-  baseURL: '/api/v1',
+  baseURL: 'https://makeup-api.herokuapp.com/api/v1',
 });
 
 //get product request width params
